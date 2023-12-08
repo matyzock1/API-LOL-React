8aa1cc3f7b85c06ee886eba84b5633a0dfb6df06
Don duck <patriciozapatahumada@gmail.com>
2023-12-07 21:22:26 -0300

    search

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <header>
-        <SearchComponent></SearchComponent>
       </header>
       <ChampionList></ChampionList>
     </div>
--- src/components/ChampionList.js
+++ src/components/ChampionList.js
@@ -1,9 +1,12 @@
 // ChampionList.js
 import React, { useState, useEffect } from 'react';
 import './ChampionList.css';
+import SearchComponent from './SearchComponent';
 
 const ChampionList = () => {
   const [champions, setChampions] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -26,17 +29,22 @@ const ChampionList = () => {
   return (
     <div className="champion-list container">
       <h1 className="heading">Lista de Nombres de Campeones</h1>
+      <SearchComponent searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       <div className="list">
-        {champions.map(champion => (
-          <div key={champion.id} className="listItem">
-            <img
-              src={`http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/${champion.image}`}
-              alt={champion.name}
-              className="champion-image"
-            />
-            <div>{champion.name}</div>
-          </div>
-        ))}
+        {champions
+          .filter(champion => 
+            champion.name.toLowerCase().includes(searchTerm.toLowerCase()))
+          .map(champion => (
+            <div key={champion.id} className="listItem">
+              <img
+                src={`http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/${champion.image}`}
+                alt={champion.name}
+                className="champion-image"
+              />
+              <div>{champion.name}</div>
+            </div>
+          ))
+        }
       </div>
     </div>
   );
